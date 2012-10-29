5a979772c1d3075bac1363ccd0ae860e2eae909f
Matthew Andrews <matthew@andrews.eu.com>
2012-10-29 12:27:03 +0000

    - APP_ROOT set by api/resources

--- api/resources/index.php
+++ api/resources/index.php
@@ -12,7 +12,20 @@ $js = $js . file_get_contents('../../source/datastores/indexeddb.js');
 $js = $js . file_get_contents('../../source/datastores/websql.js');
 $js = $js . file_get_contents('../../source/templates.js');
 $js = $js . file_get_contents('../../source/appcache.js');
-$js = $js . '}(APP));';
+$js = $js . '}(APP)),';
+
+// Detect and set the absolute path to the root of the web app
+// First get a clean version of the current directory (will include api/resources)
+$appRoot = trim(dirname($_SERVER['SCRIPT_NAME']), '/');
+
+// Strip of api/resources from the end of the path
+$appRoot = trim(preg_replace('/api\/resources$/i', '', $appRoot), '/');
+
+// Ensure the path starts and ends with a slash or just / if on the root of domain
+$appRoot = '/' . ltrim($appRoot . '/', '/');
+
+$js = $js . ' APP_ROOT = "' . $appRoot . '";';
+
 $output['js'] = $js;
 
 // Concatenate the files in the /css/ directory
